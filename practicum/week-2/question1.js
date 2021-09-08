a81efa4eefe2885b7fb7854a70d6bb08586eac7b
Matthew Gleason <matt.gleason007@gmail.com>
2021-09-08 12:58:25 -0700

    Question 1 finished

--- practicum/week-2/question1.js
+++ practicum/week-2/question1.js
@@ -5,6 +5,15 @@ For example given the input array [5, 6, 1], the running sum is
 [5, 5+6, 5+6+1]
 */
 
-var runningSum = function(input) {
-   
+var runningSum = function (input) {
+  let output = [];
+  let sum = 0;
+  for (var i = 0; i < input.length; i++) {
+    sum += input[i];
+    output.push(sum);
+  }
+  return output;
 };
+
+let q1test = runningSum([8, 5, 6, 1]);
+console.log(q1test);
