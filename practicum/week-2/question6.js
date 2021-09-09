b7ba252920d8a250422893cce0d465cd98fbdbf1
Matthew Gleason <matt.gleason007@gmail.com>
2021-09-09 13:43:15 -0700

    Question 6

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -5,6 +5,17 @@ input: [1, 2, 2, 3, 4, 7, 9]
 output: 3 
 */
 
-var median = function(input) {
-
+var median = function (input) {
+  if (input.length === 0) {
+    return 0;
+  }
+  input.sort((a, b) => a - b);
+  let middle = Math.floor(input.length / 2);
+  if (input.length % 2) {
+    return input[middle];
+  }
+  return (input[middle - 1] + input[middle]) / 2;
 };
+
+let test1 = median([1, 2, 3, 4, 6, 6, 7, 8]);
+console.log(test1);
