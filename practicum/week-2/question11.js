08dc877c1e51bdb1217ba5166a7ccc88af3433d7
Matthew Gleason <matt.gleason007@gmail.com>
2021-09-10 01:52:04 -0700

    Question 8, 11, 12

--- practicum/week-2/question11.js
+++ practicum/week-2/question11.js
@@ -9,6 +9,17 @@ input: [2, 5, 4, 2] (this means in the encoded list we will have two 5's and fou
 output: [5, 5, 2, 2, 2, 2]
 */
 
-var decode = function(input) {
-
+var decode = function (input) {
+  let output = [];
+  var i = 0;
+  while (i < input.length) {
+    for (var j = 0; j < input[i]; j++) {
+      output.push(input[i + 1]);
+    }
+    i += 2;
+  }
+  return output;
 };
+
+let test11 = decode([0, 8, 1, 3, 2, 7]);
+console.log(test11);
--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -7,4 +7,20 @@ input: [23, 44, 12, 4]
 output: [1, 0, 1, 2]
 */
 
-var biggerAndEven = function (input) {};
+var biggerAndEven = function (input) {
+  let output = [];
+  for (var i = 0; i < input.length; i++) {
+    var num = 0;
+    for (var j = 0; j < input.length; j++) {
+      if (input[j] % 2 == 0 && input[j] > input[i]) {
+        num++;
+      }
+    }
+    output.push(num);
+  }
+
+  return output;
+};
+
+let test8 = biggerAndEven([1, 3, 4, 2, 6, 8, 5, 7]);
+console.log(test8);
